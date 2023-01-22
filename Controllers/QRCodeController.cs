e598d999314bba3136786b987bc9a437c138be20
YogeshHadiya <hadiyayk33@gmail.com>
2023-01-22 14:05:54 +0530

    Add project files.

---
+++ Controllers/QRCodeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCodeInASPNetCore.Models;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using static QRCoder.PayloadGenerator;
+
+namespace QRCodeInASPNetCore.Controllers
+{
+    public class QRCodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            QRCodeModel model = new QRCodeModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(QRCodeModel model)
+        {
+
+            Payload payload = null;
+
+            switch (model.QRCodeType)
+            {
+                case 1: // website url
+                    payload = new Url(model.WebsiteURL);
+                    break;
+                case 2: // bookmar url
+                    payload = new Bookmark(model.BookmarkURL, model.BookmarkURL);
+                    break;
+                case 3: // compose sms
+                    payload = new SMS(model.SMSPhoneNumber, model.SMSBody);
+                    break;
+                case 4: // compose whatsapp message
+                    payload = new WhatsAppMessage(model.WhatsAppNumber, model.WhatsAppMessage);
+                    break;
+                case 5://compose email
+                    payload = new Mail(model.ReceiverEmailAddress, model.EmailSubject, model.EmailMessage);
+                    break;
+                case 6: // wifi qr code
+                    payload = new WiFi(model.WIFIName, model.WIFIPassword, WiFi.Authentication.WPA);
+                    break;
+
+            }
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload);
+            QRCode qrCode = new QRCode(qrCodeData);
+            var qrCodeAsBitmap = qrCode.GetGraphic(20);
+
+            // use this when you want to show your logo in middle of QR Code
+            //Bitmap logoImage = new Bitmap(@"wwwroot/img/Virat-Kohli.jpg");
+            //var qrCodeAsBitmap = qrCode.GetGraphic(20, Color.Black, Color.Red, logoImage);
+
+            string base64String = Convert.ToBase64String(BitmapToByteArray(qrCodeAsBitmap));
+            model.QRImageURL = "data:image/png;base64," + base64String;
+            return View("Index", model);
+        }
+
+        private byte[] BitmapToByteArray(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ Models/QRCodeModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRCodeInASPNetCore.Models
+{
+    public class QRCodeModel
+    {
+        public int QRCodeType { get; set; }
+        public string QRImageURL { get; set; }
+
+        //for bookmark qr code
+        public string BookmarkTitle { get; set; }
+        public string BookmarkURL { get; set; }
+
+        // for email qr codes
+        public string ReceiverEmailAddress { get; set; }
+        public string EmailSubject { get; set; }
+        public string EmailMessage { get; set; }
+         
+
+        //for sms qr codes
+        public string SMSPhoneNumber { get; set; }
+        public string SMSBody { get; set; }
+
+        //for website
+        public string WebsiteURL { get; set; }
+
+        // for whatsapp qr message code
+        public string WhatsAppNumber { get; set; }
+        public string WhatsAppMessage { get; set; }
+
+        // for wifi qr code
+        public string WIFIName { get; set; }
+        public string WIFIPassword { get; set; } 
+    }
+
+
+}
